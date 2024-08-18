8bd0ec10046c5ca07eb203befbff827fe62f07e2
rafacostavilla <rafaelcostavilla@gmail.com>
2024-08-18 14:56:45 +1200

    Destructure tuples

--- tuples/src/main.rs
+++ tuples/src/main.rs
@@ -4,4 +4,21 @@ fn main() {
     let (_x, y, _) = tup;
 
     println!("The value of y is: {y}");
+
+    let a = tup.0;
+    let b = tup.1;
+    let c = tup.2;
+
+
+    println!("Tuple is: {a}, {b}, {c}");
+
+    let mut new_tuple = (1, 2);
+    new_tuple.0 = 0;
+    new_tuple.1 += 5;
+
+    let a = new_tuple.0;
+    let b = new_tuple.1;
+
+    println!("{a} and {b}")
+
 }
