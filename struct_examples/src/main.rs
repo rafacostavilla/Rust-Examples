302f1cffd738eefe234c3484aa1dbca686460a6f
rafacostavilla <rafaelcostavilla@gmail.com>
2024-08-26 11:38:31 +1200

    Use Tuple Structs without named fields

--- struct_examples/src/main.rs
+++ struct_examples/src/main.rs
@@ -5,6 +5,9 @@ struct User {
     sign_in_count: u64,
 }
 
+struct Color(i32, i32, i32);
+struct Point(i32, i32, i32);
+
 fn build_user (email: String, username: String) -> User{
     User { 
         active: true,
@@ -34,4 +37,7 @@ fn main(){
     };
     //We cannot use the username from user1 because it has moved when we created user3 from the user1's username
     // println!("{}", user1.username);
+
+    let black = Color(0, 0, 0);
+    let origin = Point(0, 0, 0);
 }
