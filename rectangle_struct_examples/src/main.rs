0e0be035d11f0fb3195445bba321c222fbc7eab6
rafacostavilla <rafaelcostavilla@gmail.com>
2024-08-26 16:40:44 +1200

    Prettify Rectangle Debug Output

--- rectangle_struct_examples/src/main.rs
+++ rectangle_struct_examples/src/main.rs
@@ -1,3 +1,4 @@
+#[derive(Debug)]
 struct Rectangle{
     width: u32,
     height: u32,
@@ -13,6 +14,7 @@ fn main() {
         "The area of the rectangle is {} square pixels.",
         area(&rect1)
     );
+    println!("{rect1:#?}");
 }
 
 fn area(rectangle: &Rectangle) -> u32{
