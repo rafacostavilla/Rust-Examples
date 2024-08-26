2f2a498916a817b0b3a1ba8d21bd013baead2c57
rafacostavilla <rafaelcostavilla@gmail.com>
2024-08-26 16:59:55 +1200

    Use dbg! to create debug messages

--- rectangle_struct_examples/src/main.rs
+++ rectangle_struct_examples/src/main.rs
@@ -5,18 +5,11 @@ struct Rectangle{
 }
 
 fn main() {
+    let scale = 2;
     let rect1= Rectangle {
-        width: 30,
+        width: dbg!(30*scale),
         height: 50,
     };
 
-    println!(
-        "The area of the rectangle is {} square pixels.",
-        area(&rect1)
-    );
-    println!("{rect1:#?}");
-}
-
-fn area(rectangle: &Rectangle) -> u32{
-    rectangle.width*rectangle.height
+    dbg!(&rect1);
 }
