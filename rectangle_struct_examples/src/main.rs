63d352d364e91a0ee5c129f955b59bd881121186
rafacostavilla <rafaelcostavilla@gmail.com>
2024-08-26 17:35:37 +1200

    Create a function called width as the parameter with the same name

--- rectangle_struct_examples/src/main.rs
+++ rectangle_struct_examples/src/main.rs
@@ -8,6 +8,10 @@ impl Rectangle{
     fn area(&self) -> u32{
         self.height*self.width
     }
+
+    fn width(&self) -> bool{
+        self.width > 0
+    }
 }
 
 fn main() {
@@ -20,4 +24,8 @@ fn main() {
         "Rectangle area is {} square pixels",
         rect1.area()
     );
+
+    if rect1.width() {
+        println!("The rectangle has a nonzero width; It is {}", rect1.width);
+    }
 }
