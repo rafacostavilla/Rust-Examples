6fea398ab3e278115101283690909f5961f3e65e
rafacostavilla <rafaelcostavilla@gmail.com>
2024-08-26 17:58:07 +1200

    Create a can_hold function to check if a rectangle can hold another one

--- rectangle_struct_examples/src/main.rs
+++ rectangle_struct_examples/src/main.rs
@@ -12,12 +12,27 @@ impl Rectangle{
     fn width(&self) -> bool{
         self.width > 0
     }
+
+    fn can_hold(&self, rectangle: &Rectangle) -> bool{
+        self.width > rectangle.width && self.height > rectangle.height
+    }
 }
 
 fn main() {
     let rect1= Rectangle {
         width: 30,
         height: 50,
+    };
+    
+    
+    let rect2= Rectangle {
+        width: 20,
+        height: 40,
+    };
+    
+    let rect3= Rectangle {
+        width: 20,
+        height: 60,
     };
 
     println!(
@@ -28,4 +43,7 @@ fn main() {
     if rect1.width() {
         println!("The rectangle has a nonzero width; It is {}", rect1.width);
     }
+
+    println!("Can rect1 hold rect2: {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3: {}", rect1.can_hold(&rect3));
 }
