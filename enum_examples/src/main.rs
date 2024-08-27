b10d1618ea557c1fa3b8470e29c928c2f941e0fc
rafacostavilla <rafaelcostavilla@gmail.com>
2024-08-27 22:16:51 +1200

    Create Message Enum and call method

--- enum_examples/src/main.rs
+++ enum_examples/src/main.rs
@@ -3,8 +3,29 @@ enum IpAddrKind{
     V6(String),
 }
 
+enum Message{
+    Quit,
+    Move {x: i32, y: i32},
+    Write(String),
+    ChangeColor(i32, i32, i32),
+}
+
+impl Message{
+    fn call(&self){
+        match self {
+            Message::Write(value) => {
+                println!("{value}");
+            }
+            _ => (),
+        }
+    }   
+}
+
 fn main() {
     let _home = IpAddrKind::V4(127, 0, 0, 1);
 
     let _loopback = IpAddrKind::V6(String::from("::1"));
+
+    let message = Message::Write(String::from("hello"));
+    message.call();
 }
