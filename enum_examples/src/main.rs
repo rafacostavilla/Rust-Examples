9b2e3d99dc8a77707d39707e2b3519193ca467a8
rafacostavilla <rafaelcostavilla@gmail.com>
2024-08-27 21:34:42 +1200

    Remove struct to store enum data, and use only enum to do that

--- enum_examples/src/main.rs
+++ enum_examples/src/main.rs
@@ -1,21 +1,10 @@
 enum IpAddrKind{
-    V4,
-    V6,
-}
-
-struct  IpAddr {
-    _kind: IpAddrKind,
-    _address: String,
+    V4(String),
+    V6(String),
 }
 
 fn main() {
-    let _home = IpAddr{
-        _kind: IpAddrKind::V4,
-        _address: String::from("127.0.0.1"),
-    };
+    let _home = IpAddrKind::V4(String::from("127.0.0.1"));
 
-    let _loopback = IpAddr{
-        _kind: IpAddrKind::V6,
-        _address: String::from("::1"),
-    };
+    let _loopback = IpAddrKind::V6(String::from("::1"));
 }
